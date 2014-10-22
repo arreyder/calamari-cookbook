7ae5879042ea83b44485fed3d4cc933c5202bcf2
Chris Rhodes <crrhodes@godaddy.com>
2014-10-22 07:54:28 -0700

    initial commit of basic package install stuff...

---
+++ CHANGELOG.md
@@ -0,0 +1,13 @@
+calamari CHANGELOG
+==================
+
+This file is used to list changes made in each version of the calamari cookbook.
+
+0.1.0
+-----
+- [your_name] - Initial release of calamari
+
+- - -
+Check the [Markdown Syntax Guide](http://daringfireball.net/projects/markdown/syntax) for help with Markdown.
+
+The [Github Flavored Markdown page](http://github.github.com/github-flavored-markdown/) describes the differences between markdown on github and standard markdown.
---
+++ recipes/default.rb
@@ -0,0 +1,34 @@
+#
+# Cookbook Name:: calamari
+# Recipe:: default
+#
+# Copyright 2014, YOUR_COMPANY_NAME
+#
+# All rights reserved - Do Not Redistribute
+#
+#
+include_recipe 'salt::master'
+include_recipe 'apache2::mod_wsgi'
+include_recipe 'ceph::install'
+
+%w(libcairo2 supervisor python-cairo libpq5 postgresq salt-minion python-sqlalchemy python-greenlet python-twisted python-txamqp python-gevent dctrl-tools debconf-utils python-support).each do |pkg|
+  package pkg do
+    action :install
+  end
+end  
+
+apt_package "calamri-server" do
+  action :install
+  source '/root/calamari-server_1.2.1-64-g32d231e_amd64.deb'
+end
+
+apt_package "calamri-clients" do
+  action :install
+  source '/root/calamari-clients_1.2.1.1-29-g3790c24_all.deb'
+end
+
+apt_package "diamond" do
+  action :install
+  source '/root/diamond_3.4.67_all.deb'
+end
+
