9cbd97ba811f0f68b85f59637eb91a4cde37cda3
Chris Rhodes <crrhodes@godaddy.com>
2014-10-28 08:46:43 -0700

    one more try

--- recipes/default.rb
+++ recipes/default.rb
@@ -3,6 +3,6 @@ include_recipe "apt"
 apt_repository 'calamari' do
   uri          'http://chef-server.cloud.dev.phx3.gdg:3389/'
   arch         'amd64'
-  components   ['main', 'stable']
+  components   ['stable', 'main']
   key          'http://chef-server.cloud.dev.phx3.gdg:3389/myrepo.gpg'
 end
