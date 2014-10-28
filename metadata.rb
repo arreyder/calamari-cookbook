eda66cae14944635596409f4bfc3ff95c4bce0c3
Chris Rhodes <crrhodes@godaddy.com>
2014-10-28 07:33:16 -0700

    we need the apt recipe to add the repo

--- metadata.rb
+++ metadata.rb
@@ -6,6 +6,7 @@ description      'Installs/Configures calamari'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          '0.1.0'
 
+depends 'apt'
 depends 'salt'
 depends 'apache2'
 depends 'ceph'
